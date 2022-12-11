e5d6b66940ea9392628ecd20d421cf8bbc2941bb
FilipHojan <81771162+FilipHojan@users.noreply.github.com>
2022-12-11 01:43:45 +0100

    4 zmiana gif

--- gra 1/Form6.Designer.cs
+++ gra 1/Form6.Designer.cs
@@ -29,7 +29,19 @@ namespace gra_1
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::gra_1.Properties.Resources._5e5ba12c9e1837a9171a75e1ec638c8e;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 333);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form6
             // 
@@ -37,12 +49,17 @@ namespace gra_1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::gra_1.Properties.Resources._5e5ba12c9e1837a9171a75e1ec638c8e;
             this.ClientSize = new System.Drawing.Size(500, 347);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form6";
             this.Text = "Form6";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
